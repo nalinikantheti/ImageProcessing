722dc8c845431d35047d0b29649f932ee57c8c48
nalinikantheti <nalinikantheti@gmail.com>
2022-10-25 19:44:41 -0400

    test two

--- src/ImageUtil.java
+++ src/ImageUtil.java
@@ -7,7 +7,7 @@ import java.io.FileInputStream;
  * This class contains utility methods to read a PPM image from file and simply print its contents. Feel free to change this method 
  *  as required.
  */
-public class ImageUtil {
+public class ImageUtil  {
 
   /**
    * Read an image file in the PPM format and print the colors.
